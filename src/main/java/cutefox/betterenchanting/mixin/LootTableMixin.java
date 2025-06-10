cc3255bdbdbecdf08c550622ecbc43ca84471a56
TheCuteFox <joachim.vanoni@gmail.com>
2025-06-10 13:58:18 +0200

    Redo loot generation for fishing.

--- src/main/java/cutefox/betterenchanting/mixin/LootTableMixin.java
+++ src/main/java/cutefox/betterenchanting/mixin/LootTableMixin.java
@@ -6,15 +6,20 @@ import cutefox.betterenchanting.BetterEnchanting;
 import cutefox.betterenchanting.Util.ModEnchantmentHelper;
 import cutefox.betterenchanting.datagen.ModEnchantIngredientMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.context.LootContextParameterSet;
+import net.minecraft.loot.context.LootContextParameters;
+import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,6 +27,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.ArrayList;
@@ -52,6 +58,22 @@ public abstract class LootTableMixin {
 
         this.betterEnchanting$addEssences(inventory, random);
         this.essences.clear();
+    }
+
+    @Inject(method = "generateLoot(Lnet/minecraft/loot/context/LootContext;)Lit/unimi/dsi/fastutil/objects/ObjectArrayList;", at= @At(value = "RETURN"),locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    public void betterEnchanting$replaceBookFromFishing(LootContext context, CallbackInfoReturnable<ObjectArrayList<ItemStack>> cir, ObjectArrayList objectArrayList){
+
+        ObjectArrayList<ItemStack> list = objectArrayList.clone();
+
+        if(context.get(LootContextParameters.THIS_ENTITY) instanceof FishingBobberEntity ){
+            if (list.get(0).getItem().equals(Items.ENCHANTED_BOOK)){
+                ItemStack bookIngredient = ModEnchantmentHelper.replaceEnchantedBook(list.get(0));
+                list.clear();
+                list.add(bookIngredient);
+                cir.setReturnValue(list);
+            }
+        }
+
     }
 
     private void betterEnchanting$addEssences(Inventory inventory, Random random){
--- src/main/java/cutefox/betterenchanting/registry/ModItemTags.java
+++ src/main/java/cutefox/betterenchanting/registry/ModItemTags.java
@@ -11,10 +11,12 @@ public class ModItemTags {
 
     public static final TagKey<Item> ENCHANTEMNT_INGREDIENT = TagKey.of(RegistryKeys.ITEM, Utils.id("enchantment_ingredients"));
     public static final TagKey<Item> ENCHANTMENT_ESSENCE = TagKey.of(RegistryKeys.ITEM, Utils.id("enchantment_essence"));
+    public static final TagKey<Item> VANILLA_ESSENCE = TagKey.of(RegistryKeys.ITEM, Utils.id("vanilla_essence"));
     public static final TagKey<Item> LIBRARIAN_RARE_INGREDIENT = TagKey.of(RegistryKeys.ITEM, Utils.id("librarian_rare_ingredient"));
     public static final TagKey<Item> NEOENCHANT_ESSENCE = TagKey.of(RegistryKeys.ITEM, Utils.id("neoenchant_essence"));
     public static final TagKey<Item> BUMBLEZONE_ESSENCE = TagKey.of(RegistryKeys.ITEM, Utils.id("bumblezone_essence"));
     public static final TagKey<Item> SPELL_POWER_ESSENCE = TagKey.of(RegistryKeys.ITEM, Utils.id("spell_engine_essence"));
+    public static final TagKey<Item> NOVA_ESSENCE = TagKey.of(RegistryKeys.ITEM, Utils.id("nova_essence"));
 
     public static final TagKey<Item> IRON_UPGRADE_TEMPLATE = TagKey.of(RegistryKeys.ITEM, Identifier.of("fox_den","iron_upgrade_template"));
     public static final TagKey<Item> DIAMOND_UPGRADE_TEMPLATE = TagKey.of(RegistryKeys.ITEM, Identifier.of("fox_den","diamond_upgrade_template"));
